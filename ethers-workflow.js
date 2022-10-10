9229a077c0029d10fe0db9d30769989719e75fc8
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-09 23:45:42 -0400

    Added 'provider' comments/notes

--- ethers-workflow.js
+++ ethers-workflow.js
@@ -1,6 +1,6 @@
 const ethers = require("ethers");
 const solc = require("solc");
 const Contract = ethers.Contract;
-const provider = ethers.getDefaultProvider("ropsten");
+const provider = ethers.getDefaultProvider("ropsten"); // this is just a connection to the network, the EVM. for querying and sending state changes
 const fs = require("fs-extra");
 
