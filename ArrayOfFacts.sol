754e625be4ea827c2dc6024505cc6754164c982d
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-09 23:57:25 -0400

    Create ArrayOfFacts.sol

---
+++ ArrayOfFacts.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT  
+
+pragma solidity ^0.8.7;
+
+contract ArrayOfFacts {
+    string[] private facts;
+    address private owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+}
