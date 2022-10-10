560feccfa355fdd6e8d08251046a24a9a48060a6
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-10 00:26:32 -0400

    Update ethers-workflow.js

--- ethers-workflow.js
+++ ethers-workflow.js
@@ -1,6 +1,12 @@
 const ethers = require("ethers");
 const solc = require("solc");
 const Contract = ethers.Contract;
-const provider = ethers.getDefaultProvider("ropsten"); // this is just a connection to the network, the EVM. for querying and sending state changes
+const provider = ethers.getDefaultProvider("ropsten"); // this is just a connection to the network, the EVM
+//for querying and sending state changes
 const fs = require("fs-extra");
 
+//Create a function that readds a file and returns its content.
+
+function readFile(fileName) {
+    return fs.readFileSync(fileName, "utf8"); ///Ask Jason what exactly this means/does?
+}
